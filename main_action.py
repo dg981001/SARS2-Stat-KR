22d1d668f7b74837ca752a237bf5289bcfc3f814
unknown <dg981001@naver.com>
2020-03-14 00:29:42 +0900

    change message

--- main_action.py
+++ main_action.py
@@ -48,16 +48,16 @@ if __name__=="__main__":
 
     readme = open('README.md', mode='wt', encoding='utf-8')
     readme.write('''
-## 대한민국 우한 코로나 바이러스 감염자 실시간 통계
+## 우한 코로나 바이러스 대한민국 확진자 실시간 통계
+#### China's Wuhan Corona-virus Koran confirmed case chart
 {0} KST
-
-** 부산, 경상북도 홈페이지에 표시된 수치가 질병관리본부 발표보다 적습니다. **
 
 각 **시/도/구/군청** 사이트에 공개되어 있는 **공식 자료**를 수합하여 만든 통계입니다.
 질병관리본부에서 발표되는 공식 수치와는 다를 수 있습니다.
 
 {1}
 
+** 부산, 경상북도 홈페이지에 표시된 수치가 질병관리본부 발표보다 적습니다. **
 #1 확진자(중국인) 제외
     '''.format(kst_time(), table.generate(data)))
     readme.close()
